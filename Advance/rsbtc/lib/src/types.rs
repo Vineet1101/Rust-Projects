47ac44fadf06f2a2660f88f24f7ae197539603c8
Vineet1101 <Vineetgoel692@gmail.com>
2026-02-03 07:49:43 +0530

    completed data types chapter
    Signed-off-by: Vineet1101 <Vineetgoel692@gmail.com>

---
+++ Advance/rsbtc/lib/src/types.rs
@@ -0,0 +1,104 @@
+use crate::U256;
+use crate::crypto::{PublicKey, Signature};
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+use crate::util::MerkleRoot;
+use crate::sha256::Hash;
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct Blockchain {
+    pub blocks: Vec<Block>,
+}
+
+impl Blockchain {
+    pub fn new() -> Blockchain {
+        Blockchain { blocks: vec![] }
+    }
+
+    pub fn add_block(&mut self, block: Block) {
+        self.blocks.push(block);
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct Block {
+    pub header: BlockHeader,
+    pub transactions: Vec<Transaction>,
+}
+
+impl Block {
+    pub fn new(block_header: BlockHeader, transactions: Vec<Transaction>) -> Self {
+        Block {
+            header: block_header,
+            transactions: transactions,
+        }
+    }
+
+    pub fn hash(&self) -> ! {
+        unimplemented!()
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct BlockHeader {
+    /// Timestamp of the block
+    pub timestamp: DateTime<Utc>,
+    /// Nonce used to mine the block
+    pub nonce: u64,
+    /// Hash of the previous block
+    pub prev_block_hash: Hash,
+    /// Merkle root of the block's transactions
+    pub merkle_root: MerkleRoot,
+    ///target
+    pub target: U256,
+}
+
+impl BlockHeader {
+    pub fn new(
+        timestamp: DateTime<Utc>,
+        nonce: u64,
+        prev_block_hash: Hash,
+        merkle_root: MerkleRoot,
+        target: U256,
+    ) -> Self {
+        BlockHeader {
+            timestamp,
+            nonce,
+            prev_block_hash,
+            merkle_root,
+            target,
+        }
+    }
+
+    pub fn hash(&self) -> Hash {
+       Hash::hash(self)
+    }
+}
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct Transaction {
+    pub inputs: Vec<TransactionInput>,
+    pub outputs: Vec<TransactionOuput>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct TransactionInput {
+    pub prev_transaction_output_hash:Hash,
+    pub signature: Signature,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct TransactionOuput {
+    pub value: u64,
+    pub unique_id: Uuid,
+    pub pubkey: PublicKey,
+}
+
+impl Transaction {
+    pub fn new(inputs: Vec<TransactionInput>, outputs: Vec<TransactionOuput>) -> Self {
+        Transaction { inputs, outputs }
+    }
+    pub fn hash(&self) -> Hash {
+        Hash::hash(self)
+    }
+}
