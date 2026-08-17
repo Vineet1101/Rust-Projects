4bd781a08e1738969c2ae09b11a4968df44e58a3
Vineet1101 <Vineetgoel692@gmail.com>
2026-08-17 23:16:27 +0530

    project setup for stableswap done
    Signed-off-by: Vineet1101 <Vineetgoel692@gmail.com>

---
+++ Advance/Solana-Smart-Contracts/stableswap/programs/stableswap/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("4qXgy46E7G1ziEafdAkjra4f3KZCqWxpgRhiEQykgcHq");
+
+#[program]
+pub mod stableswap {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
